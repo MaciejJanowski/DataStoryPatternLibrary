69924f32238ea8a5e63a1a15a3dd915161c7278f
MaciejJanowski <maciej.janowski@insight-centre.org>
2019-07-07 18:29:41 +0100

    IC,PO,EC with documentation

--- datastories.py
+++ datastories.py
@@ -200,3 +200,105 @@ class DataStoryPatterns():
         elif(order_type=="desc"):
             return dataframe.sort_values(by=columns_to_order, ascending=False).head(number_of_records)
 
+    def InternalComparison(self,cube=[],dims=[],meas=[],hierdims=[],df=pd.DataFrame(), dim_to_compare="",meas_to_compare="",comp_type=""):# TODO
+        """
+        InternalComparison - comparison of numeric values related to textual values within one column
+        ...
+        Attributes
+        --------------
+        cube: str
+            Cube to retrieve data
+        dims: list[str]
+            list of Strings (dimension names) to retrieve
+        meas: list[str]
+            list of measures to retrieve
+        hierdims: dict{hierdim:{"selected_level":[value]}}
+            hierarchical dimension (if provided) to retrieve data from specific
+            hierarchical level
+        df: dataframe
+            if data is already retrieved from SPARQL endpoint, dataframe itself can
+            be provided
+        dim_to_compare: str
+            dimension, which textual values are bound to be investigated
+        meas_to_compare: str
+            measure(numeric column), which values related to dim_to_compare 
+            will be processed
+        comp_type: str
+            type of comparison to be performed
+        ...
+        Output
+        -----------
+        Independent from comp_type selected, output data will have additional column with numerical
+        column processed in specific way.
+        Available comparison types (comp_type):
+            diffmax->difference with max value related to specific textual value
+            diffmean->difference with arithmetic mean related to specific textual value
+            diffmin->difference with minimum value related to specific textual value
+
+        """
+        
+        if(dim_to_compare and meas_to_compare):
+            dim_to_compare=dims[0]
+            meas_to_compare=meas[0]
+        if(df.empty):
+            dataframe=self.retrieveData(cube,dims,meas,hierdims)
+        else:
+            dataframe=df
+        if(comp_type=="sum"):
+            return dataframe.groupby(dim_to_compare)[meas_to_compare].sum(axis=1)
+        elif(comp_type=="diffmax"):
+            dataframe["DiffMax"]=dataframe.groupby(dim_to_compare)[meas_to_compare].transform(lambda x: x-x.max())
+            return dataframe
+        elif(comp_type=="diffmean"):
+            dataframe["DiffMean"]=dataframe.groupby(dim_to_compare)[meas_to_compare].transform(lambda x: x-round(x.mean(),2))
+            return dataframe
+        elif(comp_type=="diffmin"):
+            dataframe["DiffMin"]=dataframe.groupby(dim_to_compare)[meas_to_compare].transform(lambda x: x-x.min())
+            return dataframe
+
+
+    def ProfileOutliers(self,cube=[],dims=[],meas=[],hierdims=[],df=pd.DataFrame(), displayType="outliers_only"):
+        """
+        ProfileOutliers - detection of unusual values within data (anomalies)
+        ....
+        Attributes
+        --------------
+        cube: str
+            Cube to retrieve data
+        dims: list[str]
+            list of Strings (dimension names) to retrieve
+        meas: list[str]
+            list of measures to retrieve
+        hierdims: dict{hierdim:{"selected_level":[value]}}
+            hierarchical dimension (if provided) to retrieve data from specific
+            hierarchical level
+        df: dataframe
+            if data is already retrieved from SPARQL endpoint, dataframe itself can
+            be provided
+        displayType: str
+            what values are bound to be displayed
+        ...
+        Output
+        -----------
+        Based on displayType
+            outliers_only->returns rows from dataset where unusual values 
+                            were detected
+            without_outliers->returns dataset with excluded rows where unusual 
+                            values were detecetd
+        
+        """
+        if(df.empty):
+            dataframe=self.retrieveData(cube,dims,meas,hierdims)
+        else:
+            dataframe=df
+        
+        noOutliers=dataframe[(np.abs(stats.zscore(dataframe.select_dtypes(exclude=["object"]))) < 3).all(axis=1)]
+
+        outliersDF=pd.concat([dataframe,noOutliers]).drop_duplicates(keep=False, inplace=False)
+        
+        if(displayType=="outliers_only"):
+            return outliersDF
+        elif(displayType=="without_outliers"):
+            return noOutliers
+
+    def ExternalComparison(self,cube=[],dims=[],meas=[],hierdims=[],df=pd.DataFrame(),dims_to_compare=[],meas_to_compare="",comp_type=""):
