9f7cf246aaa37ceb52effb61c4e190a5c4a26a38
MaciejJanowski <maciej.janowski@insight-centre.org>
2019-07-09 22:10:13 +0100

    0.1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='datastories',
-      version='0.1.1.5',
+      version='0.1.1.6',
       url='https://github.com/MaciejJanowski/DataStoryPatternLibrary',
       license='MIT',
       author='Maciej Janowski',
